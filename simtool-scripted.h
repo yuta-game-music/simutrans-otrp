f3a24143e5efa86f70fe3e402db66b955524b558
Dwachs <dwachs@gmx.net>
2020-12-11 09:33:46 +0000

    FIX tooltips for scripted tools (THLeaderH)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9498 8aca7d54-2c30-db11-9de9-000461428c89

--- simtool-scripted.h
+++ simtool-scripted.h
@@ -93,7 +93,7 @@ public:
 
 	const char *work(player_t* player, koord3d pos) OVERRIDE;
 
-	using exec_script_base_t::get_tooltip;
+	const char *get_tooltip(const player_t *pl) const OVERRIDE { return exec_script_base_t::get_tooltip(pl); }
 };
 
 class tool_exec_two_click_script_t : public two_click_tool_t, public exec_script_base_t {
@@ -125,7 +125,7 @@ public:
 
 	virtual image_id get_marker_image() const OVERRIDE;
 
-	using exec_script_base_t::get_tooltip;
+	const char *get_tooltip(const player_t *pl) const OVERRIDE { return exec_script_base_t::get_tooltip(pl); }
 };
 
 #endif
