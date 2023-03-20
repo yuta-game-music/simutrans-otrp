c28271c7b71a5a54c44c678d5ab0e5ba9bf5d694
teamhimeH <thleaderh@gmail.com>
2023-03-20 21:24:49 +0900

    increment OTRP_VERSION_PATCH

--- simversion.h
+++ simversion.h
@@ -29,7 +29,7 @@
 
 #define OTRP_VERSION_MAJOR 33
 #define OTRP_VERSION_MINOR 2
-#define OTRP_VERSION_PATCH 3
+#define OTRP_VERSION_PATCH 4
 // NOTE: increment OTRP_VERSION_MAJOR when the save data structure changes.
 
 #define MAKEOBJ_VERSION "60.5"
