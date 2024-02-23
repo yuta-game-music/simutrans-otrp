0ab7c5e27bd06dad1f44cbfda3cd842606cc5548
Yuta <yunaomaki708@gmail.com>
2024-02-23 23:29:31 +0900

    ログ削除

--- network/network_cmd_ingame.cc
+++ network/network_cmd_ingame.cc
@@ -1751,7 +1751,6 @@ bool nwc_service_t::execute(karte_t *welt)
 			case DETAIL_TYPE_HALT:
 			{
 				halthandle_t halt = haltestelle_t::get_alle_haltestellen()[index];
-				dbg->error("network_cmd_ingame", "halt[%i] %s", index, halt->get_name());
 				print_int_json_value(&buf, "index", index);
 				print_string_json_value(&buf, "name", halt->get_name());
 				print_int_json_value(&buf, "owner_number", halt->get_owner()->get_player_nr());
@@ -1761,7 +1760,6 @@ bool nwc_service_t::execute(karte_t *welt)
 				for (uint8 goods_type = 0; goods_type < goods_count; goods_type++)
 				{
 					const goods_desc_t* goods_desc = goods_manager_t::get_info(goods_type);
-					dbg->error("network_cmd_ingame", "goods[%i] %s", goods_type, goods_desc->get_name());
 					if (!halt->is_enabled(goods_desc)) continue;
 					print_object_start_json_value(&buf);
 					print_int_json_value(&buf, "index", goods_type);
@@ -1775,7 +1773,6 @@ bool nwc_service_t::execute(karte_t *welt)
 					for (int connection_index = 0; connection_index < connections_count; connection_index++)
 					{
 						haltestelle_t::connection_t connection = connections[connection_index];
-						dbg->error("network_cmd_ingame", "connections[%i] %s", connection_index, connection.halt->get_name());
 						print_object_start_json_value(&buf);
 						print_int_json_value(&buf, "index", connection_index);
 						print_int_json_value(&buf, "halt_index", connection.halt.get_id());
