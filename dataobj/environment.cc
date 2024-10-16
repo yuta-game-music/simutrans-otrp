2b38a7c4e12f025e51dff4f8db2990bf085b3c43
danivenk <danivenk@gmail.com>
2024-10-16 23:09:53 +0200

    forgotten to remove redundant env_t variables

--- dataobj/environment.cc
+++ dataobj/environment.cc
@@ -13,7 +13,6 @@
 #include "../sys/simsys.h"
 #include "../simmesg.h"
 #include "koord3d.h"
-#include "../simcity.h"
 
 #include "../utils/simrandom.h"
 void rdwr_win_settings(loadsave_t *file); // simwin
@@ -112,8 +111,6 @@ bool env_t::hide_trees;
 uint8 env_t::hide_buildings;
 bool env_t::hide_under_cursor;
 uint16 env_t::cursor_hide_range;
-bool env_t::highlight_city;
-stadt_t *env_t::highlighted_city;
 bool env_t::use_transparency_station_coverage;
 uint8 env_t::station_coverage_show;
 sint32 env_t::show_names;
@@ -231,9 +228,6 @@ void env_t::init()
 	hide_buildings = env_t::NOT_HIDE;
 	hide_under_cursor = false;
 	cursor_hide_range = 5;
-	
-	highlight_city = false;
-	highlighted_city = NULL;
 
 	scroll_infinite = false;
 
