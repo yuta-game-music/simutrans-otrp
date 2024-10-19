65378da03bd1459464adc40be94ee3bab16829bc
teamhimeH <thleaderh@gmail.com>
2024-10-19 14:58:41 +0900

    rename REVERSE_COUPLING

--- dataobj/schedule_entry.h
+++ dataobj/schedule_entry.h
@@ -47,8 +47,8 @@ public:
 		UNLOAD_ALL        = 1U << 5, // The convoy unloads all loads here.
 		LOAD_BEFORE_DEP   = 1U << 6, // The convoy loads just before the departure.
 		TRANSFER_INTERVAL = 1U << 7,
-		REVERSE_CONVOY	= 1U << 8,// convoy reverses the order of its vehicles.
-		REVERSE_convoi_coupling		= 1U << 9,// The convoy reverse the parent-child relationship of the convoy coupling.
+		REVERSE_CONVOY	  = 1U << 8, // convoy reverses the order of its vehicles.
+		REVERSE_COUPLING  = 1U << 9, // The convoy reverses the parent-child relationship of the convoy coupling.
 	};
 
 	/**
@@ -140,8 +140,8 @@ public:
 
 	bool is_reverse_convoy() const { return (stop_flags&REVERSE_CONVOY)>0; }
 	void set_reverse_convoy(bool y) { y ? stop_flags |= REVERSE_CONVOY : stop_flags&= ~REVERSE_CONVOY; }
-	bool is_reverse_convoi_coupling() const { return (stop_flags&REVERSE_convoi_coupling)>0; } 
-	void set_reverse_convoi_coupling(bool y) { y ? stop_flags |= REVERSE_convoi_coupling : stop_flags &= ~REVERSE_convoi_coupling; }
+	bool is_reverse_convoi_coupling() const { return (stop_flags&REVERSE_COUPLING)>0; } 
+	void set_reverse_convoi_coupling(bool y) { y ? stop_flags |= REVERSE_COUPLING : stop_flags &= ~REVERSE_COUPLING; }
 	uint8 get_stop_flag2() const { return stop_flags>>8; }
 	void set_stop_flag2(uint8 f) { stop_flags = (f<<8)+(stop_flags&0x00FF); }
 	
