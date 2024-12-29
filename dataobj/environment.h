704c75f761be9a4b64641a0fef44303f6bb85b42
danivenk <danivenk@gmail.com>
2024-12-29 01:24:48 +0100

    added comment to env_t header entry

--- dataobj/environment.h
+++ dataobj/environment.h
@@ -314,6 +314,7 @@ public:
 	/// Hide buildings and trees within range of mouse cursor
 	static uint16 cursor_hide_range;
 
+	/// Which city is highlighted
 	static stadt_t* highlighted_city;
 
 	/// color used for cursor overlay blending
